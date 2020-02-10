c3e904140035b242bc0ca1b0bcf565bc6588e5a9
nirjhar18 <nirjhar18@gmail.com>
2020-02-10 12:29:16 -0600

    Changes to Config File

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_qRPkGsp5w', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4jl521llljmdgqensllt1m8mm0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
